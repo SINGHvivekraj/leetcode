be0033572b77cbbde7c9eec09204bd30d77b5906
viveksingh <viveks98765@gmail.com>
2025-08-15 11:45:01 +0530

    solved using top down recursion + memoization appraoch

---
+++ medium problems/longest_palindromic_subsequence.cpp
@@ -0,0 +1,20 @@
+//https://leetcode.com/problems/longest-palindromic-subsequence/
+class Solution {
+public:
+    int solve(string& s,int i,int j,vector<vector<int>> &dp){
+        if(i>j)return 0;
+        if(i==j)return 1;
+        if(dp[i][j]!=-1)return dp[i][j];
+        else if(s[i]==s[j]){
+            return dp[i][j] = 2+solve(s,i+1,j-1,dp);
+        }
+        else {
+            return dp[i][j] = max(solve(s,i,j-1,dp),solve(s,i+1,j,dp));
+        }
+    }
+    int longestPalindromeSubseq(string s) {
+        int n=s.size();
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+        return solve(s,0,n-1,dp);
+    }
+};
